16e8d20e5afc64ad96bbee24788b300fc29b0abb
nikhovas <nikhovas@ya.ru>
2020-03-30 13:34:37 +0500

    build

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 
+case $1 in
+ "debug") BUILD_FOLDER="./cmake-build-debug" ;;
+ *)       BUILD_FOLDER="./cmake-build" ;;
+esac
+SOURCE_FOLDER="."
+
+cmake -B ${BUILD_FOLDER} -S ${SOURCE_FOLDER}
+cd ${BUILD_FOLDER}
+make
+cd ..
